626dbe429fce70c853a22b0793f3e74ea884dab1
xiaoyishan <xiaoyishan1992@163.com>
2017-05-17 11:27:51 +0800

    Add files via upload

---
+++ NSDictionary+Json.h
@@ -0,0 +1,16 @@
+//
+//  NSDictionary+Json.h
+//  toJson
+//
+//  Created by Sundear on 2017/5/17.
+//  Copyright © 2017年 xiexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Json)
+
+-(NSString*)Json;
+-(NSString*)JsonPretty;
+
+@end
